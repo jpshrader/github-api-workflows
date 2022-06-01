1cd1378f21ddce8b342c95874816265692c38be1
Jack Shrader <96661942+jpshrader@users.noreply.github.com>
2022-06-01 05:32:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 '''Application entry point'''
 import settings_constants as constants
 
-from file_helper import get_file, JSON_FILE_TYPE
 from arg_helper import parse_args
 from interpreter import interpret_instructions
+from file_helper import get_file, JSON_FILE_TYPE
 
 from github_services.github_service_factory import get_github_service
 
