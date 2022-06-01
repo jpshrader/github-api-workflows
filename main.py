f26084301cbb7abbbbdc2a5f08fdb823c9412f83
Jack Shrader <96661942+jpshrader@users.noreply.github.com>
2022-05-31 22:20:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 '''Application entry point'''
 import settings_constants as constants
 
-from file_helper import get_file
+from file_helper import get_file, JSON_FILE_TYPE
 from arg_helper import parse_args
 
 from github_services.github_service_factory import get_github_service
@@ -9,5 +9,5 @@ from github_services.github_service_factory import get_github_service
 if __name__ == '__main__':
     args = parse_args()
 
-    settings = get_file(constants.SETTINGS_FILE, args.data_type)
+    settings = get_file(constants.SETTINGS_FILE, JSON_FILE_TYPE)
     github = get_github_service(args.access_token, settings[constants.ITEMS_PER_PAGE])
