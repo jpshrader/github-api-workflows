1c31cc740cd9c2610a48e817bc6fed4f8fc695d6
Jack Shrader <96661942+jpshrader@users.noreply.github.com>
2022-06-01 02:49:24 -0400

    sigh

--- arg_helper.py
+++ arg_helper.py
@@ -3,6 +3,7 @@ import argparse
 
 from file_helper import get_file, get_data, YAML_FILE_TYPE
 
+BASE_PATH_ARG = 'base_path'
 FILE_TYPE_ARG = 'file_type'
 INSTRUCTIONS_ARG = 'instructions'
 ACCESS_TOKEN_ARG = 'access_token'
@@ -11,17 +12,20 @@ class CommandLineArgs: # pylint: disable=too-few-public-methods
     '''Command line argument object'''
     data: object
     data_type: str
+    base_path: str
     access_token: str
 
-    def __init__(self, data: object, data_type: str, access_token: str):
+    def __init__(self, data: object, data_type: str, base_path: str, access_token: str):
         self.data = data
         self.data_type = data_type
+        self.base_path = base_path
         self.access_token = access_token
 
 def parse_args() -> CommandLineArgs:
     '''Retrieves command line arguments to this python application'''
     parser = argparse.ArgumentParser()
 
+    parser.add_argument(f'--{BASE_PATH_ARG}', default='', type=str)
     file_type_action = parser.add_argument(f'--{FILE_TYPE_ARG}', default='', type=str)
     instruction_action = parser.add_argument(f'--{INSTRUCTIONS_ARG}', default='', type=str)
     access_token_action = parser.add_argument(f'--{ACCESS_TOKEN_ARG}', default='', type=str)
@@ -49,7 +53,7 @@ def parse_args() -> CommandLineArgs:
     if access_token == '':
         raise_required_arg_error(access_token_action, ACCESS_TOKEN_ARG)
 
-    return CommandLineArgs(data, file_type, access_token)
+    return CommandLineArgs(data, file_type, args[BASE_PATH_ARG], access_token)
 
 def raise_required_arg_error(action, arg_name):
     '''Returns a new ArgumentError for a given argument'''
--- main.py
+++ main.py
@@ -9,5 +9,5 @@ from github_services.github_service_factory import get_github_service
 if __name__ == '__main__':
     args = parse_args()
 
-    settings = get_file(constants.SETTINGS_FILE, JSON_FILE_TYPE)
+    settings = get_file(args.base_path + constants.SETTINGS_FILE, JSON_FILE_TYPE)
     github = get_github_service(args.access_token, settings[constants.ITEMS_PER_PAGE])
