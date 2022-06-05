8431c4502e9f330ed938595c61b97e2c62d75003
Jack Shrader <96661942+jpshrader@users.noreply.github.com>
2022-06-05 01:13:28 -0400

    Update branch_management.py

--- github_functions/branch_management.py
+++ github_functions/branch_management.py
@@ -1,9 +1,10 @@
 '''Branch Management Service'''
 from datetime import datetime
-from github import Github, Branch, Repository
+from github import Github, Branch, Repository, Label
 
 from github_services.comparison_service import is_branch_ahead
 from github_services.repo_service import get_repo_by_full_name
+from github_services.label_service import get_labels_from_repo
 from github_services.pull_request_service import create_pull_request_by_repo
 from github_services.branch_service import delete_branch_from_repo_by_branch, get_branch_from_list, create_branch_from_repo
 
@@ -17,7 +18,14 @@ def merge_branch_and_pr(github: Github, repo_full_name: str, from_branch: str, t
     pr_name = f'Merge {from_branch} to {to_branch}'
     pull_request = create_pull_request_by_repo(repo, pr_name, '', to_branch, new_branch_name, is_draft=False)
 
-    pull_request.set_labels(labels)
+    labels_to_add: list[Label.Label] = []
+    repo_labels = get_labels_from_repo(repo)
+    for label in labels:
+        for repo_label in repo_labels:
+            if label == repo_label.name:
+                labels_to_add.append(repo_label)
+    pull_request.set_labels(labels_to_add)
+
     pull_request.create_review_request(reviewers)
 
 def identify_empty_branches(github: Github, repo_full_name: str, target_branch: str) -> list[Branch.Branch]:
